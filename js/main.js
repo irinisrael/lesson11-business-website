bce0c8d04b84e852a2b09f669c6c08a5e9a821c5
Irin Israel <irinisrael@icloud.com>
2015-11-10 18:07:27 -0500

    fixed js, buttons issue, photos repeating, just need to clean up

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
-$(document).ready(function() {
+$('.button-index').click(function() {
+	alert('We look forward to working with you!');
+});
 
-	$('button').click(function() {
-		alert('We look forward to working with you!');
-	});
+$('.many-trucks-module').click(function() {
+	$('.many-trucks-module').slideUp(1000);
+});
 
-	$('.many-trucks-module').click(function() {
-		$('.many-trucks-module').slideUp(1000);
-	});
+
+(function() {
+		var documentEl = $(document),
+			backgroundContainer = $('div#background-container');
+
+			documentEl.on("scroll", function() {
+				var currScrollPos = documentEl.scrollTop();
+				backgroundContainer.css('background-position', '0' + -currScrollPos + 'px');
+			});
+			//slider
+$(function() {
+	//Configure
+	var width = 520;
+	var animationSpeed = 1000;
+	var pause = 4000;
+	var currentSlide = 1;
+
+	//Cache
+	var $slider = $('#image_slider');
+	var $slideContainer = $slider.find('.slider_wrapper');
+	var $slider_wrapper = $slideContainer.find('.slide');
+
+	setInterval(function(){
+		$slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function() {
+			currentSlide++
+			if(currentSlide === $slider_wrapper.length);
+				currentSlide = 1;
+				// $slideContainer.css('margin-left' 0);
+		});
+
+	}, pause);
 
 })
+
+})();
